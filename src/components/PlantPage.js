b11b2ef04e80fc9cefb894aadb405201bc2d38ef
Jared Patterson <jpwesley34@gmail.com>
2024-02-15 01:26:55 -0800

    completes core deliverables and adds Vileplume.

--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([]);
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    fetch(`http://127.0.0.1:6001/plants`)
+    .then(res => res.json())
+    .then(data => {
+      setPlants(data)})
+  }, [])
+
+  function onAddPlant(newPlant){
+    setPlants([...plants, newPlant])
+  }
+
+  const plantsToDisplay = plants
+  .filter((plant) => plant.name.toLowerCase().includes(search.toLowerCase()))
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm 
+      onAddPlant={onAddPlant}/>
+      <Search whileSearching = {setSearch}/>
+      <PlantList 
+      plants = {plantsToDisplay}/>
     </main>
   );
 }
